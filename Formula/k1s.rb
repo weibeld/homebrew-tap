145bd3e6cc27713b88353c320c78325d47e60711
Daniel Weibel <danielmweibel@gmail.com>
2020-07-08 16:19:18 +0200

    Update to k1s 0.1.2

--- Formula/k1s.rb
+++ Formula/k1s.rb
@@ -1,8 +1,8 @@
 class K1s < Formula
   desc "The world's simplest Kubernetes dashboard (50 lines of Bash code)"
   homepage "https://github.com/weibeld/k1s"
-  url "https://github.com/weibeld/k1s/releases/download/0.1.1/k1s-0.1.1.tar.gz"
-  sha256 "21399a4bc4aad70810f3912042a031fb4c344db2e128b3883cbbe1ed092f72cc"
+  url "https://github.com/weibeld/k1s/releases/download/0.1.2/k1s-0.1.2.tar.gz"
+  sha256 "447bd602c1d486a47ebedd88f2c3f4c872ed68139bf47cdff1b2347c5d1c5312"
 
   bottle :unneeded
 
