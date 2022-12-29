efea730a1fc1db0275e1312243ceb970cbc979f3
Daniel Weibel <danielmweibel@gmail.com>
2022-12-29 14:15:47 +0100

    Fix code style issues detected by test-bot
    See test-bot [1] which is run automatically by GitHub Actions. Issued
    fixed by running
    
        brew audit --strict --online k1s
    
    locally and manually fixing the reported issues.
    
    [1] https://docs.brew.sh/Brew-Test-Bot

--- Formula/k1s.rb
+++ Formula/k1s.rb
@@ -1,5 +1,5 @@
 class K1s < Formula
-  desc "The world's simplest Kubernetes dashboard (50 lines of Bash code)"
+  desc "Kubernetes dashboard implemented with 50 lines of Bash code"
   homepage "https://github.com/weibeld/k1s"
   url "https://github.com/weibeld/k1s/releases/download/0.1.2/k1s-0.1.2.tar.gz"
   sha256 "447bd602c1d486a47ebedd88f2c3f4c872ed68139bf47cdff1b2347c5d1c5312"
@@ -9,6 +9,6 @@ class K1s < Formula
   end
 
   test do
-    system "k1s -v"
+    system "k1s", "-v"
   end
 end
